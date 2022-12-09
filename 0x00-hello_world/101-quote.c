2cd52e10fd16c120475a5d3dd642cb031a3d7b43
Newbie1203 <princeopoku737@gmail.com>
2022-12-08 20:37:52 -0800

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - print and that piece of art is useful\" - Dora Korpar, 2015-10-19
+ * and a new line 
+ *
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (0);
+}
