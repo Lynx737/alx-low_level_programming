5cbc8f96a975cf01b75afb9893db5cd0d7272d3e
Newbie1203 <princeopoku737@gmail.com>
2022-12-09 21:08:51 -0800

    new

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit numbers
+ *
+ * Desciption: print single digit numbers
+ *
+ * Return:0 code successfull
+ */
+
+int main(void)
+{
+	int d=0;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
