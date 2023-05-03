22c27d15fd25d55beb030751bda911e2826b4e22
Lynx737 <89715457+Lynx737@users.noreply.github.com>
2023-05-03 04:48:22 +0000

    linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,9 +9,9 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i;
+	unsigned int a;
 
-	for (i = 0; i < index && head != NULL; i++)
+	for (a = 0; a < index && head != NULL; a++)
 	{
 		head = head->next;
 	}
