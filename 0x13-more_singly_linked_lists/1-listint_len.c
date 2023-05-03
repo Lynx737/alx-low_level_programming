e40e81726c36666adfac9fed9678d0acbc997519
Lynx737 <89715457+Lynx737@users.noreply.github.com>
2023-05-03 03:27:20 +0000

    linked lists

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,18 +3,18 @@
 /**
  * listint_len - returns the number of elements in
  * a linked list.
- * @h: head of a list.
+ * @h: head of the list(linked).
  *
  * Return: numbers of nodes.
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t nnodes = 0;
+	size_t node = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		h = h->next;
-		nnodes++;
+		node++;
 	}
-	return (nnodes);
+	return (node);
 }
